6d32c56d8d076bf8aae478776fe5ef65297c1d9f
ihsavru <vurvashi11@gmail.com>
2017-07-09 14:03:34 +0530

    add contact links

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#title').on('click',function(){
+        $(this).animate({left: '155px', top: '155px'}, 1000);
+        $('#left').animate({ height: '230px'}, 1000);
+        $('#right').animate({ height: '245px', left: "230px"}, 1000);
+        $('#top').animate({ width: '230px'}, 1000);
+        $('#bottom').animate({ top: '230px', width: "245px"}, 1000);
+        $('.links').animate({width: '300px', left: '90%', top: '90%'}, 1500);
+    });
+})

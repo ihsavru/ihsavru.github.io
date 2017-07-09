fdf313b293e12a788f5e32e6c84925d66be03a92
ihsavru <vurvashi11@gmail.com>
2017-07-09 17:03:46 +0530

    style changes

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ $(function(){
         $('#right').animate({ height: '245px', left: "230px"}, 1000);
         $('#top').animate({ width: '230px'}, 1000);
         $('#bottom').animate({ top: '230px', width: "245px"}, 1000);
-        $('.links').animate({width: '300px', left: '90%', top: '90%'}, 1500);
+        $('.links').animate({width: '300px',left: '50%', top: '95%'}, 1000);
     });
 })

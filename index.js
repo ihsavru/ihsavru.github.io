8705945406b6935445b9d628b1bbaf7657622c91
ihsavru <vurvashi11@gmail.com>
2017-07-09 22:58:00 +0530

    change style

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 $(function(){
-    $('#title').on('click',function(){
-        $(this).animate({left: '155px', top: '155px'}, 1000);
-        $('#left').animate({ height: '230px'}, 1000);
-        $('#right').animate({ height: '245px', left: "230px"}, 1000);
-        $('#top').animate({ width: '230px'}, 1000);
-        $('#bottom').animate({ top: '230px', width: "245px"}, 1000);
-        $('.links').animate({width: '300px',left: '50%', top: '95%'}, 1000);
+    $('.menu').on('click',function(){
+        $('#title').animate({left: '100px', top: '100px', fontSize: '100px'}, 1000);
+        $('#list').animate({fontSize: '100px', left: '50%'}, 1000);
     });
-})
+    $('#theme').on('click', function(){
+    
+    })
+});

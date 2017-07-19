ea4b2d2db1431352dda4fda26fa4e057872ff1e9
ihsavru <vurvashi11@gmail.com>
2017-07-19 23:32:26 +0530

    update work.html

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
 $(function(){
     $('.menu').on('click',function(){
         $('.content').fadeOut('slow');
+        $('#up').fadeOut('slow');
+        $('#base').fadeOut('slow');
+        $('#side').fadeOut('slow');
         $('#backdrop').fadeOut('slow');
         $('#title').animate({left: '60px', top: '60px', fontSize: '30px', lineHeight: '30px'}, 1000);
         $('#list').animate({fontSize: '10vh', left: '50%', lineHeight: '10vh'}, 1000);
     });
     $('#about').on('click', function(){
-        $('#work').fadeOut('slow');
-        $('#art').fadeOut('slow');
-        $('#about').animate({
-            letterSpacing: '5vw',
-            fontSize: '5vw'
-        }, 1000);
         $('#about').css({
-            'text-decoration': 'line-through'
+            'text-decoration': 'line-through',
+            'color' :'rgb(253, 199, 0)'
         });
         $('#up').fadeOut('slow');
         $('#side').fadeOut('slow');
-        $('#about').fadeOut('slow');
         setTimeout(function(){window.location.href = "about.html";}, 1200);
     });
     $('#work').on('click', function(){
         $('#up').fadeOut('slow');
-        $('#work').animate({
-            letterSpacing: '5vw',
-            fontSize: '5vw'
-        }, 1000);
         $('#work').css({
-            'text-decoration': 'line-through'
+            'text-decoration': 'line-through',
+            'color' :'rgb(253, 199, 0)'
         });
         $('#base').fadeOut('slow'); 
-        $('#list').fadeOut('slow');
         setTimeout(function(){window.location.href = "work.html";}, 1200);
     });
     $('#art').on('click', function(){
-        $('#work').fadeOut('slow');
-        $('#art').animate({
-            letterSpacing: '5vw',
-            fontSize: '5vw'
-        }, 1000);
         $('#base').fadeOut('slow');
         $('#side').fadeOut('slow');
         $('#art').css({
-            'text-decoration': 'line-through'
+            'text-decoration': 'line-through',
+            'color' :'rgb(253, 199, 0)'
         });
-        $('#list').fadeOut('slow');
         setTimeout(function(){window.location.href = "art.html";}, 1200);
     });
     $('#title').on('click', function(){
@@ -73,15 +61,21 @@ $(function(){
 
     $('#first').on('click',function(){
         $('#avatar').animate({opacity: 1}, 500);
-        $('#education > img').animate({opacity:'0'}, 500);
-        $('#education > div').animate({opacity:'0'}, 500);
-    })
+        $('#education').animate({opacity:'0'}, 500);
+        $('#language').animate({opacity:'0'}, 500);
+    });
 
     $('#second').on('click',function(){
         $('#avatar').animate({opacity: 0}, 500);
-        $('#education > img').animate({opacity:'1'}, 500);
-        $('#education > div').animate({opacity:'1'}, 500);
-    })
+        $('#education').animate({opacity:'1'}, 500);
+        $('#language').animate({opacity:'0'}, 500);
+    });
+
+    $('#third').on('click',function(){
+        $('#avatar').animate({opacity: 0}, 500);
+        $('#education').animate({opacity:'0'}, 500);
+        $('#language').animate({opacity:'1'}, 500);
+    });
 });
 $(window).on('load',function(){
             $('.preloader').fadeOut();

71a2fa9b901d2a79542407f454f64ba7c54e8c35
ihsavru <vurvashi11@gmail.com>
2019-06-29 23:57:04 +0530

    add a menu on mobz

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,10 @@
 window.onload = () => {
   let logo = document.createElement('div');
-  logo.innerHTML = `<h1 class="nav-title">IHSAVRU</h1>`;
+  logo.innerHTML = `
+  <div class="s-menu">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 3c0-0.6-0.4-1-1-1H1C0.4 2 0 2.4 0 3v2c0 0.6 0.4 1 1 1h22c0.6 0 1-0.4 1-1V3z"/><path d="M24 11c0-0.6-0.4-1-1-1H1c-0.6 0-1 0.4-1 1v2c0 0.6 0.4 1 1 1h22c0.6 0 1-0.4 1-1V11z"/><path d="M24 19c0-0.6-0.4-1-1-1H1c-0.6 0-1 0.4-1 1v2c0 0.6 0.4 1 1 1h22c0.6 0 1-0.4 1-1V19z"/></svg>
+  </div>
+  <h1 class="nav-title">IHSAVRU</h1>`;
   logo.setAttribute('id', 'logo');
 
   let menu = document.createElement('div');
@@ -39,4 +43,9 @@ window.onload = () => {
   </li>
   `
   document.getElementById('contact').append(contact);
+
+  document.getElementsByClassName('s-menu')[0].onclick = () => {
+    menu.classList.toggle('menu-out');
+    let links = menu.children;
+  }
 }
